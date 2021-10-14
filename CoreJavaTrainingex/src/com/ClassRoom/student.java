744b0339d9f117f03f251ea74172f081bfae6faf
pace <pace@pace-PC>
2021-10-14 15:51:32 +0530

    second commit

--- CoreJavaTrainingex/src/com/ClassRoom/student.java
+++ CoreJavaTrainingex/src/com/ClassRoom/student.java
@@ -2,4 +2,28 @@ package com.ClassRoom;
 
 public class student {
 
+	private int a;
+	private float b;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public float getB() {
+		return b;
+	}
+	public void setB(float b) {
+		this.b = b;
+	}
+	public student(int a, float b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	@Override
+	public String toString() {
+		return "student [a=" + a + ", b=" + b + "]";
+	}
+	
 }
